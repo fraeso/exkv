45f5c6aa142ccc09eb6f018c512c5cb288849ce4
Austin Sofaer <164747892+fraeso@users.noreply.github.com>
2026-07-25 02:17:46 +0700

    refactor: change agents to use genserver

--- lib/bucket.ex
+++ lib/bucket.ex
@@ -1,33 +1,62 @@
 defmodule KV.Bucket do
-  use Agent
+  use GenServer
 
   @doc """
   Starts a new bucket - defines init state
   """
   def start_link(opts) do
-    Agent.start_link(fn -> %{} end, opts)
+    GenServer.start_link(__MODULE__, %{}, opts)
   end
 
   @doc """
   Gets a value from the `bucket` by `key`
   """
   def get(bucket, key) do
-    Agent.get(bucket, &Map.get(&1, key))
+    GenServer.call(bucket, {:get, key})
   end
 
   @doc """
   Puts K/V to the bucket
   """
   def put(bucket, k, v) do
-    Agent.update(bucket, &Map.put(&1, k, v))
+    GenServer.call(bucket, {:put, k, v})
   end
 
   @doc """
   Deletes `k` from `bucket` along with it's associated value.
-  
+
   Returns the current value of `key` if it exists.
   """
   def delete(bucket, k) do
-    Agent.get_and_update(bucket, &Map.pop(&1, k))
+    GenServer.call(bucket, {:delete, k})
   end
+
+  ### GenServer Callbacks
+  @impl true
+  def init(bucket) do
+    state = %{
+      bucket: bucket
+    }
+
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_call({:get, key}, _from, state) do
+    value = get_in(state.bucket[key])
+    {:reply, value, state}
+  end
+
+  def handle_call({:put, key, value}, _from, state) do
+    state = put_in(state.bucket[key], value)
+    {:reply, :ok, state}
+  end
+
+  def handle_call({:delete, key}, _from, state) do
+    {value, state} = pop_in(state.bucket[key])
+    {:reply, value, state}
+  end
+    
+
+  
 end
