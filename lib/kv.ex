f91a8e5c21966c9e3b949eae6ed764e3df5fa2df
Austin Sofaer <164747892+fraeso@users.noreply.github.com>
2026-07-24 21:31:25 +0700

    feat: use dynamic supervisor to create and lookup buckets at runtime

--- lib/kv.ex
+++ lib/kv.ex
@@ -4,9 +4,20 @@ defmodule KV do
   @impl true
   def start(_start_type, _start_args) do
     children = [
-      {Registry, keys: :unique, name: KV.Registry}
+      {Registry, keys: :unique, name: KV.Registry},
+      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one}
     ]
 
-    Supervisor.start_link(children, strategy: :one_for_one, name: KV.Supervisor)
+    Supervisor.start_link(children, strategy: :one_for_one, name: KV.RootSupervisor)
   end
+
+  def create_bucket(name) do
+    DynamicSupervisor.start_child(KV.BucketSupervisor, {KV.Bucket, name: via(name)})
+  end
+
+  def lookup_bucket(name) do
+    GenServer.whereis(via(name))
+  end
+
+  defp via(name), do: {:via, Registry, {KV.Registry, name}}
 end
