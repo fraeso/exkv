35eb67099785e096a031bb6af6a0c288df97fcfe
Austin Sofaer <164747892+fraeso@users.noreply.github.com>
2026-07-24 14:26:25 +0700

    test: store and get from named process

--- test/bucket_test.exs
+++ test/bucket_test.exs
@@ -8,4 +8,17 @@ defmodule KV.BucketTest do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+  test "stores values by key on a named process", config do
+    test_uuid = "1386279e-d508-4de0-a25c-e8aedbe83d2b"
+
+    {:ok, _} = KV.Bucket.start_link(name: config.test)
+    assert KV.Bucket.get(config.test, test_uuid) == nil
+
+    KV.Bucket.put(config.test, test_uuid, true)
+    assert KV.Bucket.get(config.test, test_uuid) == true
+
+    KV.Bucket.put(config.test, test_uuid, false)
+    assert KV.Bucket.get(config.test, test_uuid) == false
+  end
 end
